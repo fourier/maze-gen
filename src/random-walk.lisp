9f83d8b39226d94c37dbea34ccebfc4053a26f85
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2019-04-22 22:35:47 +0200

    Updated random-walk to include constraints

--- src/random-walk.lisp
+++ src/random-walk.lisp
@@ -1,21 +1,33 @@
 (in-package :maze-gen)
 
-(defun random-walk (from finish-if &key (allow-loops nil))
+(defun random-walk (from finish-if &key (allow-loops nil)
+                         (constraint-if
+                          (lambda (c)
+                            (declare (ignore c)) nil)))
   "Create random path on a grid from the element FROM
 until the predicte FINISH-IF is t on next element of
 the generated random path. The cell on which the
 FINISH-IF predicate is true is also added to the end
-of the result list"
+of the result list
+CONSTRAINT-IF is a predicate specifying if the cell
+shall be removed from the list of candidates to walk to"
   (flet ((walk (from)
-           (random-elt (remove-if #'null (cell-neighbours from)))))
+           (let ((cells (remove-if (lambda (c)
+                                     (or (null c)
+                                         (funcall constraint-if c)))
+                                   (cell-neighbours from))))
+                 (random-elt cells))))
     (loop with result = (list from)
           for current = (car result)
           for next = (walk current)
-          until (funcall finish-if next)
+          until (or (null next) (funcall finish-if next))
           do
           ;; loop avoidance
           (let ((mem (member next result)))
             (if (and mem (not allow-loops))
                 (setf result mem)
                 (push next result)))
-          finally (return (nreverse (push next result))))))
+          finally
+             (progn
+               (when next (push next result))
+               (return (nreverse result))))))
